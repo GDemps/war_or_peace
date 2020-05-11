a5560329c23c5b338e8f6078d5850ab9ef7ead8a
Grant Dempsey <CryptoDemps@gmail.com>
2020-05-11 03:05:19 -0600

    tested_game_works

--- test/game_test.rb
+++ test/game_test.rb
@@ -7,6 +7,17 @@ require './lib/game'
 
 class DeckTest < Minitest::Test
 
-
+def test_it_exists_with_attributes
+  card1 = Card.new(:heart, 'Jack', 11)
+  card2 = Card.new(:heart, '10', 10)
+  deck1 = Deck.new(card1)
+  deck2 = Deck.new(card2)
+  player1 = Player.new("G", deck1)
+  player2 = Player.new("F", deck2)
+  game1 = Game.new(player1, player2)
+  require "pry"
+  binding.pry
+  assert_instance_of Game, game1
+end
 
 end
